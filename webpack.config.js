76502495273abb246e39f43c02dcd5a6e15bc51a
Stephan Cilliers <stephanus.cilliers@gmail.com>
2017-01-16 18:26:52 +0200

    Changed public directory and built

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: '/DDM/',
     filename: 'build.js'
   },
   module: {
