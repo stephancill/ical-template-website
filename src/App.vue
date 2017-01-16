2923044dabcd79b113f28a7b1aedfd16851be06c
Stephan Cilliers <stephanus.cilliers@gmail.com>
2017-01-16 16:33:28 +0200

    Maps to ics nested array

--- src/App.vue
+++ src/App.vue
@@ -19,167 +19,231 @@
 </template>
 
 <script>
-import ical from 'ical-generator'
+import ical from 'ical.js'
+import FileSaver from 'file-saver'
+
 export default {
     name: 'app',
     data () {
         return {
-            days:
-                [
-                    {
-                        name: 'Monday',
-                        lessons: [
-                            {
-                                name: 'L1',
-                                value: ''
-                            }, {
-                                name: 'L2',
-                                value: ''
-                            }, {
-                                name: 'Utility',
-                                value: ''
-                            }, {
-                                name: 'L3',
-                                value: ''
-                            }, {
-                                name: 'L4',
-                                value: ''
-                            }, {
-                                name: 'L5',
-                                value: ''
-                            }, {
-                                name: 'L6',
-                                value: ''
-                            }, {
-                                name: 'L7',
-                                value: ''
-                            }
-                        ]
-                    }, {
-                        name: 'Tuesday',
-                        lessons: [
-                            {
-                                name: 'L1',
-                                value: ''
-                            }, {
-                                name: 'L2',
-                                value: ''
-                            }, {
-                                name: 'Utility',
-                                value: ''
-                            }, {
-                                name: 'L3',
-                                value: ''
-                            }, {
-                                name: 'L4',
-                                value: ''
-                            }, {
-                                name: 'L5',
-                                value: ''
-                            }, {
-                                name: 'L6',
-                                value: ''
-                            }, {
-                                name: 'L7',
-                                value: ''
-                            }
-                        ]
-                    }, {
-                        name: 'Wednesday',
-                        lessons: [
-                            {
-                                name: 'L1',
-                                value: ''
-                            }, {
-                                name: 'L2',
-                                value: ''
-                            }, {
-                                name: 'Utility',
-                                value: ''
-                            }, {
-                                name: 'L3',
-                                value: ''
-                            }, {
-                                name: 'L4',
-                                value: ''
-                            }, {
-                                name: 'L5',
-                                value: ''
-                            }, {
-                                name: 'L6',
-                                value: ''
-                            }, {
-                                name: 'L7',
-                                value: ''
-                            }
-                        ]
-                    }, {
-                        name: 'Thursday',
-                        lessons: [
-                            {
-                                name: 'L1',
-                                value: ''
-                            }, {
-                                name: 'L2',
-                                value: ''
-                            }, {
-                                name: 'Utility',
-                                value: ''
-                            }, {
-                                name: 'L3',
-                                value: ''
-                            }, {
-                                name: 'L4',
-                                value: ''
-                            }, {
-                                name: 'L5',
-                                value: ''
-                            }, {
-                                name: 'L6',
-                                value: ''
-                            }, {
-                                name: 'L7',
-                                value: ''
-                            }
-                        ]
-                    }, {
-                        name: 'Friday',
-                        lessons: [
-                            {
-                                name: 'L1',
-                                value: ''
-                            }, {
-                                name: 'L2',
-                                value: ''
-                            }, {
-                                name: 'L3',
-                                value: ''
-                            }, {
-                                name: 'L4',
-                                value: ''
-                            }, {
-                                name: 'L5',
-                                value: ''
-                            }, {
-                                name: 'L6',
-                                value: ''
-                            }, {
-                                name: 'Assembly',
-                                value: ''
-                            }
-                        ]
-                    }
-                ]
+            templateEndpoint: 'https://raw.githubusercontent.com/stephancill/ical-template-website/master/src/assets/2017_Blank_Timetable.ics',
+            days: [
+                {
+                    name: 'Monday',
+                    lessons: [
+                        {
+                            name: 'L1',
+                            value: ''
+                        }, {
+                            name: 'L2',
+                            value: ''
+                        }, {
+                            name: 'Utility',
+                            value: ''
+                        }, {
+                            name: 'L3',
+                            value: ''
+                        }, {
+                            name: 'L4',
+                            value: ''
+                        }, {
+                            name: 'L5',
+                            value: ''
+                        }, {
+                            name: 'L6',
+                            value: ''
+                        }, {
+                            name: 'L7',
+                            value: ''
+                        }
+                    ]
+                }, {
+                    name: 'Tuesday',
+                    lessons: [
+                        {
+                            name: 'L1',
+                            value: ''
+                        }, {
+                            name: 'L2',
+                            value: ''
+                        }, {
+                            name: 'Utility',
+                            value: ''
+                        }, {
+                            name: 'L3',
+                            value: ''
+                        }, {
+                            name: 'L4',
+                            value: ''
+                        }, {
+                            name: 'L5',
+                            value: ''
+                        }, {
+                            name: 'L6',
+                            value: ''
+                        }, {
+                            name: 'L7',
+                            value: ''
+                        }
+                    ]
+                }, {
+                    name: 'Wednesday',
+                    lessons: [
+                        {
+                            name: 'L1',
+                            value: ''
+                        }, {
+                            name: 'L2',
+                            value: ''
+                        }, {
+                            name: 'Utility',
+                            value: ''
+                        }, {
+                            name: 'L3',
+                            value: ''
+                        }, {
+                            name: 'L4',
+                            value: ''
+                        }, {
+                            name: 'L5',
+                            value: ''
+                        }, {
+                            name: 'L6',
+                            value: ''
+                        }, {
+                            name: 'L7',
+                            value: ''
+                        }
+                    ]
+                }, {
+                    name: 'Thursday',
+                    lessons: [
+                        {
+                            name: 'L1',
+                            value: ''
+                        }, {
+                            name: 'L2',
+                            value: ''
+                        }, {
+                            name: 'Utility',
+                            value: ''
+                        }, {
+                            name: 'L3',
+                            value: ''
+                        }, {
+                            name: 'L4',
+                            value: ''
+                        }, {
+                            name: 'L5',
+                            value: ''
+                        }, {
+                            name: 'L6',
+                            value: ''
+                        }, {
+                            name: 'L7',
+                            value: ''
+                        }
+                    ]
+                }, {
+                    name: 'Friday',
+                    lessons: [
+                        {
+                            name: 'L1',
+                            value: ''
+                        }, {
+                            name: 'L2',
+                            value: ''
+                        }, {
+                            name: 'L3',
+                            value: ''
+                        }, {
+                            name: 'L4',
+                            value: ''
+                        }, {
+                            name: 'L5',
+                            value: ''
+                        }, {
+                            name: 'L6',
+                            value: ''
+                        }, {
+                            name: 'Assembly',
+                            value: ''
+                        }
+                    ]
+                }
+            ]
         }
     },
     methods: {
-        getSubjects(event) {
-            const lessons = []
-            this.days.filter(day => lessons.push(...day.lessons.map(lesson => lesson.name)));
-            console.log(lessons);
+        getSubjects() {
+            let lessons = []
+            this.days.filter(day => lessons.push(...day.lessons.map(lesson => lesson.value)));
+            this.fetchTemplateFrom(this.templateEndpoint, (template) => {
+                // Use retrieved ics
+                let icsJson = this.parseTemplate(template);
+                lessons = lessons.reverse()
+                const icsLessons = []
+                for (var i = icsJson[2].length-1; i > 0; i--) {
+                    const icsName = icsJson[2][i][1][11][3]
+                    if (icsName === "CONFIRMED") {
+                        // Some cases index 11 is the status and the actual event name is in 12
+                        icsJson[2][i][1][12][3] = lessons[i-1]
+                        // console.log(icsJson[2][i][1][12][3], lessons[i-1]);
+                    } else {
+                        icsJson[2][i][1][11][3] = lessons[i-1]
+                        // console.log(icsJson[2][i][1][11][3], lessons[i-1]);
+                    }
+                }
+
+                // for (var i = icsJson[2].length-1; i > 0; i--) {
+                //     const icsName = icsJson[2][i][1][11][3]
+                //     console.log("testings");
+                //     if (icsName === "CONFIRMED") {
+                //         // Some cases index 11 is the status and the actual event name is in 12
+                //         // icsJson[2][i][1][12][3] = lessons[i-1])
+                //         console.log(icsJson[2][i][1][12][3]);
+                //     } else {
+                //         // icsJson[2][i][1][11][3] = lessons[i-1])
+                //         console.log(icsJson[2][i][1][11][3]);
+                //     }
+                // }
+                console.log(flatten(icsJson));
+                for (var i = 0; i < lessons.length; i++) {
+                    // console.log(lessons[i], icsLessons[i]);
+                }
+            });
         },
+        parseTemplate(template) {
+            let jcalData = ical.parse(template)
+            return jcalData
+        },
+        fetchTemplateFrom(endpoint, callback) {
+            let ics = ""
+            fetch(endpoint)
+              .then(blob => blob.text())
+              .then(data => { ics = data; callback(ics) });
+        }
 
     }
+}
+
+function flatten(arr, result = []) {
+  for (let i = 0, length = arr.length; i < length; i++) {
+    const value = arr[i]
+    if (Array.isArray(value)) {
+      for (let i = 0, length = value.length; i < length; i++) {
+        const value2 = value[i]
+        if (Array.isArray(value2)) {
+          flatten(value2, result)
+        } else {
+          result.push(value2)
+        }
+      }
+    } else {
+      result.push(value)
+    }
+  }
+  return result
 }
 </script>
 
